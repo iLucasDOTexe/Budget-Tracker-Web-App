a7fec00db58221666218740294dbd62881e59c96
iLucasDOTexe <83580422+iLucasDOTexe@users.noreply.github.com>
2023-10-28 14:24:30 +0200

    add 404 message

--- Backend/projectBalanceApp.js
+++ Backend/projectBalanceApp.js
@@ -4,6 +4,11 @@ const  app = express();
 
 app.use(express.static('/home/lucaspi/Project-Balance/Frontend'));
 
+app.use((req, res) => {
+    res.status(404);
+    res.send('<h1>Congratulations. You searched for a side, which does not exist (404)</h1>');
+});
+
 app.listen(3000, () => {
     console.log("App listening on port 3000");
 })
