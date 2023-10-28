fb8c22b374e1429a666ae7ae0695e04ae474c1ab
iLucasDOTexe <83580422+iLucasDOTexe@users.noreply.github.com>
2023-10-28 14:17:54 +0200

    change node relative path to absolute path

--- Backend/projectBalanceApp.js
+++ Backend/projectBalanceApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const  app = express();
 
-app.use(express.static('../Project-Balance/Frontend'));
+app.use(express.static('/home/lucaspi/Project-Balance/Frontend'));
 
 app.listen(3000, () => {
     console.log("App listening on port 3000");
