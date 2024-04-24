a04b01ffc678582869e5e39fd80c46ba2d115a8d
iLucasDOTexe <83580422+iLucasDOTexe@users.noreply.github.com>
2024-04-24 20:22:37 +0200

    refactor app file

--- Backend/projectBalanceApp.js
+++ Backend/projectBalanceApp.js
@@ -2,19 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('/home/lucaspi/Project-Balance/Frontend'));
+app.use(express.json('/home/lucaspi/Project-Balance/Frontend'));
 
 app.use((req, res) => {
     res.status(404);
     res.send('<h1>Congratulations. You searched for a side, which does not exist (404)</h1>');
 });
 
+app.listen(4444, '192.168.178.60', () => {
+    console.log("App listening on port 4444");
+})
+
 app.get('/testapi', (req, res) => {
     res.status(200).send({
         status: '200 - Das hat geklappt!'
     });
 });
-
-app.listen(4444, '192.168.178.60', () => {
-    console.log("App listening on port 4444");
-})
